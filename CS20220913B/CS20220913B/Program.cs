92e9fafb0763fc6c362fc03c25357f68a1ac83ed
nomicha <126227360+nomicha@users.noreply.github.com>
2025-04-16 03:50:00 +0800

    Add files via upload

---
+++ CS20220913B/CS20220913B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS20220913B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("111上學期\n資一乙");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("I'm Gu.");
+            Console.ReadKey();
+
+        }
+    }
+}
